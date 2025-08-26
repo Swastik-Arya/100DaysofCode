8ecfbdf39195a2175e58da9e05c599082585a718
Swastik-Arya <swastikarya947@gmail.com>
2025-08-26 23:28:09 +0530

    Add files via upload

---
+++ pnz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any number = ");
+    scanf("%d", &a);
+    if(a>0)
+    {
+        printf("the number is positive \n");
+    }
+    else if(a<0)
+    {
+        printf("the number is negative \n");
+    }
+    else{
+        printf("the number is zero");
+    }
+    return 0;
+}
