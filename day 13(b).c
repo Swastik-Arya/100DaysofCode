123fe980bcc0a4caaf5b2a9434ae8702df972fe6
Swastik-Arya <swastikarya947@gmail.com>
2025-11-28 21:14:02 +0530

    Create day 13(b).c

---
+++ day 13(b).c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    char operation;
+    
+    printf("Enter first number\n");
+    scanf("%d",&a);
+    
+    printf("Enter second number\n");
+    scanf("%d",&b);
+    
+    printf("Enter operation to be done between these two numbers\n");
+    scanf(" %c ",&operation);
+    
+    switch(operation)
+    {
+        case '+':
+        printf("%d",a+b);
+        break;
+        
+        case '-':
+        printf("%d",a-b);
+        break;
+        
+        case '*':
+        printf("%d",a*b);
+        break;
+        
+        case '/':
+        printf("%d",a/b);
+        break;
+        
+        case '%':
+        printf("%d",a%b);
+        break;
+        
+        default :
+        printf("Invalid operation");
+    }
+    
+    return 0;
+}
